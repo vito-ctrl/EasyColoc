32e6849a604cbdfe1846bf40ba63fc9c88aa3a6e
vito-ctrl <aymane.elkhadraoui1@gmail.com>
2026-02-25 05:05:10 +0000

    creation of colocation

--- app/Http/Middleware/CheckIfBanned.php
+++ app/Http/Middleware/CheckIfBanned.php
@@ -13,7 +13,7 @@ class CheckIfBanned
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    // public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next): Response
     {
         if (auth()->check() && auth()->user()->banned_at) {
             auth()->logout();
